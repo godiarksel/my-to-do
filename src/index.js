e45af99d3a5ae8acc85def48ebc4582d34127b00
godfrey <godiarksel@gmail.com>
2022-06-10 09:46:01 +0300

    Complete add, remove and edit configuration

--- src/index.js
+++ src/index.js
@@ -30,9 +30,29 @@ const addToLocalStorage = (description, completed, index) => {
   localStorage.setItem('taskList', JSON.stringify(taskArray));
 };
 
+const editTask = (task, taskContainer) => {
+  const editedTask = document.createElement('input');
+  editedTask.type = 'text';
+  editedTask.classList.add('editedTask');
+  editedTask.value = task.textContent;
+  taskContainer.replaceChild(editedTask, task);
+  editedTask.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter'){
+      const taskContainers = document.querySelectorAll('.tasks');
+      const localTasks = JSON.parse(localStorage.getItem('taskList'));
+      for (let i = 0 ; i < taskContainers.length ; i += 1){
+          localTasks[i].description = editedTask.value;
+          localStorage.setItem('taskList', JSON.stringify(localTasks));
+        }
+      taskContainer.replaceChild(task, editedTask);
+      task.textContent = editedTask.value;
+    }
+  })
+};
+
 const addTasks = (task) => {
   const tasksEl = document.createElement('div');
-  tasksEl.classList.add('tasks');
+  tasksEl.className = 'tasks';
   tasksEl.innerHTML += `
   <input type="checkbox" class="checkbox">
   <span>${task}</span>
@@ -43,37 +63,14 @@ const addTasks = (task) => {
   const checkBox = document.querySelectorAll('.checkbox');
   completeTask(checkBox);
   addToLocalStorage(task, false, checkBox.length);
-};
 
-const editTask = (task, taskContainer) => {
-  const editedTask = document.createElement('div');
-  editedTask.type = 'text';
-  editedTask.classList.add('editedTask');
-  editedTask.value = task.textContent;
-  taskContainer.replaceChild(editedTask, task);
-  editedTask.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter'){
-      const taskContainers = document.querySelectorAll('.tasks');
-      localTasks = JSON.parse(localStorage.getItem('taskList'));
-      for (let i = 0 ; i < taskContainers.length ; i++){
-        if (taskContainers[i].classList.contains('completed-tasks-container')){
-          localTasks[i].description = editedTask.value;
-          localStorage.setItem('taskList', JSON.stringify(localTasks));
-        }
-      }
-      editedTask.parentElement.classList.remove('completed-tasks-container');
-      taskContainer.replaceChild( task, editedTask);
-      task.textContent = editedTask.value;
-    }
-  })
-}
-
-const editEllip = document.querySelectorAll('.fa-ellipsis-vertical');
+  const editEllip = document.querySelectorAll('.fa-ellipsis-vertical');
   editEllip.forEach((i) => {
     i.addEventListener('click', () => {
-      editTask();
+      editTask( i.previousElementSibling,tasksEl );
     })
   })
+};
 
 taskInput.addEventListener('keypress', (e) => {
   if (e.key === 'Enter' && taskInput.value !== null) {
